d9c3a8591d034a6e538c883760f8c4f225908c4a
kimitriy <kimitriy@gmail.com>
2022-06-22 21:27:37 +0300

    m

--- Concurrency.swift
+++ Concurrency.swift
@@ -1,3 +1,46 @@
 import Foundation
 
-print("Concurrency Hello!")
+/* self.geocoder.geocodeAddressString(combinedAddress, completionHandler: {(placemarks, error) -> Void in
+	if let placemark = placemarks?[0] {
+		let placemark = placemarks![0]
+		self.placemarkLatitude = (placemark.location?.coordinate.latitude)!
+		self.placemarkLongitude = (placemark.location?.coordinate.longitude)!
+	}
+})
+let eventLatitude = self.placemarkLatitude // 0.0!
+let eventLongitude = self.placemarkLongitude // 0.0! */
+
+let num = 11
+var rv: Int = 0
+
+func myFunc(param: Int, completionHandler: (Int) -> Void) {
+	rv = param + num
+	print("in func body. rv: \(rv)")
+}
+
+myFunc(param: 10, completionHandler: {num in 
+	rv = 10 * num
+	print("in closure. rv: \(rv)")
+})
+
+let b = rv
+
+print("out of closure. b: \(b)")
+
+// 1
+/* self.geocoder.geocodeAddressString(combinedAddress, completionHandler: {(placemarks, error) -> Void in
+	// 2, some time later
+	if let placemark = placemarks?[0] {
+		// 3
+		let placemark = placemarks![0]
+		// 4
+		self.placemarkLatitude = (placemark.location?.coordinate.latitude)!
+		// 5
+		self.placemarkLongitude = (placemark.location?.coordinate.longitude)!
+		// 6
+		let eventLatitude = self.placemarkLatitude
+		// 7
+		let eventLongitude = self.placemarkLongitude
+		// and so on...
+	}
+}) */
